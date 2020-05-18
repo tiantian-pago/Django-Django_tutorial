4eb6a8ae122d6fc51d817c95f2652e37941c8d53
tiantian-pago <47686704+tiantian-pago@users.noreply.github.com>
2020-05-18 11:36:49 +0800

    增加删除书籍和添加书籍URL

--- myProject/urls.py
+++ myProject/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', views.index, name='index'),  # 新增路由映射
     path('', views.detail, name='detail'),
+    path('addBook/', views.addBook, name='addBook'),
+    path('delBook/<int:book_id>', views.deteleBook, name='delBook')
 ]
