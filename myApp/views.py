86c28b3b2313177782793a81d45328d194c3c274
tiantian-pago <47686704+tiantian-pago@users.noreply.github.com>
2020-05-18 11:33:41 +0800

    增加添加书籍和删除书籍视图函数

--- myApp/views.py
+++ myApp/views.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 # myApp/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from myApp.models import Book
+from django.urls import reverse
 
 # def index(request):
 #    return HttpResponse('Hello, world!')
@@ -11,3 +12,24 @@ def detail(request):
     book_list = Book.objects.order_by('-pub_date')[:5]
     context = {'book_list': book_list}
     return render(request, 'myApp/detail.html', context)
+
+def addBook(request):
+    if request.method == 'POST':
+        temp_name = request.POST['name']
+        temp_author = request.POST['author']
+        temp_pub_house = request.POST['pub_house']
+    
+    from django.utils import timezone
+    temp_book = Book(name=temp_name, author=temp_author, pub_house=temp_pub_house, pub_date=timezone.now())
+    temp_book.save()
+    
+    # 重定向
+    return HttpResponseRedirect(reverse('detail'))
+
+def deleteBook(request, book_id):
+    bookID = book_id
+    Book.objects.filter(id=bookID).delete()
+    
+    # 重定向
+    return HttpResponseRedirect(reverse('detail'))
+    
